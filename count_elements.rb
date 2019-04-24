25b005f2b9cdfa41b4b0ca07739feaeac71ec62d
ahare14 <alexanderhare14@gmail.com>
2019-04-24 14:11:23 -0600

    completed lab

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
+require 'pry'
+
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each do |i|
+  	if !(new_hash.key?(i))
+  		new_hash[i] = 1
+  	elsif new_hash.key?(i)
+  		new_hash[i] += 1
+    end
+  	# binding.pry	
+  end
+  new_hash
 end
  
